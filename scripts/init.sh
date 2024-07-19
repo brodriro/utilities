d280ad6b281cee12aeb6be2fadd293220cb1652d
brodriro <rzknairb@gmail.com>
2024-07-19 00:32:03 -0500

    [Add] readme & script init

---
+++ scripts/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## install Postman
+open https://www.postman.com/downloads/
+
+## install Android Studio
+open https://developer.android.com/studio?hl=es-419
+
+
+## install git
+brew install git
+
+## install OhMyZsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## install Warp terminal
+brew install --cask warp
