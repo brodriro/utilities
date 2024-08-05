501deef2d616a5547972cb0d1242ee4166455b87
brodriro <rzknairb@gmail.com>
2024-08-05 01:17:32 -0500

    [Changed] fix command theHarvester

--- scripts/ipscan.sh
+++ scripts/ipscan.sh
@@ -17,9 +17,9 @@ echo " " | $writeToFile
 echo "Search subdomains and public data for $domain ..." | $writeToFile
 if ! command -v theHarvester &> /dev/null | $writeToFile
 then
-    echo "theHarverter could not be found, pleaase install it or use Kali-Linux" | $writeToFile
+    echo "theHarvester could not be found, pleaase install it or use Kali-Linux" | $writeToFile
 else
-    theHarverter -d $domain -b baidu,dnsdumpster,subdomaincenter,yahoo,urlscan,sitedossier,rapiddns,duckduckgo,github-code -l 100 | $writeToFile
+    theHarvester -d $domain -b baidu,dnsdumpster,subdomaincenter,yahoo,urlscan,sitedossier,rapiddns,duckduckgo,github-code -l 100 | $writeToFile
 fi 
 echo "=========================================" | $writeToFile
 echo " " | $writeToFile
